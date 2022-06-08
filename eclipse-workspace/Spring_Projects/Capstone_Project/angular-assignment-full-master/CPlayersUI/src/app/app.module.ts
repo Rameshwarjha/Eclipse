2e4e561438f35b6a770fef2ef3058688c39c56da
Rameshwarjha <jharameshwar.111@gmail.com>
2022-06-08 08:03:58 +0530

    change

--- eclipse-workspace/Spring_Projects/Capstone_Project/angular-assignment-full-master/CPlayersUI/src/app/app.module.ts
+++ eclipse-workspace/Spring_Projects/Capstone_Project/angular-assignment-full-master/CPlayersUI/src/app/app.module.ts
@@ -1,12 +1,14 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import {MatIconModule} from '@angular/material/icon';
+import {MatToolbarModule} from '@angular/material/toolbar';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CplayerModule } from './cplayer/cplayer.module';
 import {MatCardModule} from '@angular/material/card';
 import { HttpClientModule } from '@angular/common/http';
 import { FormsModule } from '@angular/forms';
+import { BrowserAnimationsModule} from '@angular/platform-browser';
 
 @NgModule({
   declarations: [
@@ -18,7 +20,10 @@ import { FormsModule } from '@angular/forms';
     HttpClientModule,
     CplayerModule,
     MatCardModule,
-    FormsModule
+    FormsModule,
+    MatToolbarModule,
+    MatIconModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
